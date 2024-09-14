ec981dd0f5d788a0cc60138af503879b5d7216a5
ThiagoRoberto-dev <gestaothg2.0@gmail.com>
2024-09-13 21:33:12 -0300

    Add files via upload

---
+++ media_else_IF.py
@@ -0,0 +1,10 @@
+
+nota1=float(input("digite a 1° nota:"))
+nota2=float(input("digite a 2° nota:"))
+nota3=float(input("digite a 3° nota:"))
+media= nota1+nota2+nota3 / 3
+print("a media é", media)
+if media >= 6:
+    print("aprovado")
+else:
+    print("reprovado")
